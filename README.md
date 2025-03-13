3bade802e219381c341a5172a33f8c3692f34b05
Chayel J Heinsen <chayelheinsen@users.noreply.github.com>
2025-03-13 12:02:49 -0400

    MovableInk mParticle Sideloaded Kit [sc-145154] (#1)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# First Commit
+# MovableInk + mParticle
+
+This is a sideloaded kit to use with mParticle.
+
+Please read the [documentation](https://sdk-mobile.movableink.com/mparticle-sideloaded) to learn more.
---
+++ mParticle-MovableInk/mParticle_MovableInk.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double mParticle_MovableInkVersionNumber;
+FOUNDATION_EXPORT const unsigned char mParticle_MovableInkVersionString[];
+
+#if defined(__has_include) && __has_include(<mParticle_Appboy/MPKitAppboy.h>)
+    #import <mParticle_MovableInk/MPKitMovableInk.h>
+#else
+    #import "MPKitMovableInk.h"
+#endif
